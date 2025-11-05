830c159923c4e8f9da7d09bed3f546c0bad0b334
Aaron John Bitara <118180256+eyron29@users.noreply.github.com>
2025-11-05 09:44:58 +0800

    fix responsiveness - lost & found pages

--- src/components/LostPetCard/LostPetCard.jsx
+++ src/components/LostPetCard/LostPetCard.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router";
 function LostPetCard() {
     return (
-        <div className="w-60 h-60 flex justify-center py-2 bg-[rgba(255,255,255,0.21)] rounded-xl shadow-md">
+        <div className="w-full sm:w-56 md:w-60 h-60 flex justify-center py-2 bg-[rgba(255,255,255,0.21)] border border-white rounded-xl shadow-md z-100">
             <div className="flex flex-col gap-2 justify-between items-center">
                 <div className="w-24 h-24">
                     <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" className="w-full h-full" />
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col mt-2">
                     <p className="font-semibold">Pet Name</p>
                     <p className="font-semibold">Species</p>
                     <p className="font-semibold">Breed</p>
--- src/pages/FoundPets/FoundPets.jsx
+++ src/pages/FoundPets/FoundPets.jsx
@@ -5,32 +5,31 @@ function FoundPets() {
     return (
         <div className="flex flex-col gap-4 justify-center items-center w-full h-auto mt-8">
             <div className="flex flex-col items-center gap-8 p-8 w-full bg-[rgba(255,255,255,0.21)] rounded-3xl shadow-lg">
-                <div className="w-full h-44 flex justify-between items-center bg-[rgba(255,255,255,0.21)] rounded-xl shadow-md">
+                <div className="w-full h-auto flex justify-between items-center bg-[rgba(255,255,255,0.21)] rounded-xl shadow-md relative">
                     <div className="flex flex-col px-4 py-2 gap-2 w-2/3">
-                        <h1 className="text-6xl font-extrabold text-[#A60530]">Found Pets</h1>
-                        <p>Here you can see all the pets that have been reported missing. Check the list to see if you’ve spotted any of them, or learn more about each pet by tapping on their details. </p>
+                        <h1 className="text-3xl sm:text-4xl md:text-5x font-extrabold text-[#A60530]">Found Pets</h1>
+                        <p className='text-md sm:text-lg'>Here you can see all the pets that have been reported missing. Check the list to see if you’ve spotted any of them, or learn more about each pet by tapping on their details. </p>
                     </div>
-                    <div className="w-1/5 h-44 flex justify-center items-center">
-                        <img src={happyDogCat} alt="Happy dog & cat" className="w-full h-full" />
+                    <div className="w-1/4 md:w-1/5 h-44 md:h-32 flex justify-center items-center z-100 absolute right-0 bottom-0">
+                        <img src={happyDogCat} alt="Happy dog & cat" className="w-full h-full " />
                     </div>
                 </div>
-                <div className="w-[80%] h-auto flex justify-center items-center ">
+               <div className="w-full sm:w-[90%] h-auto flex justify-center items-center ">
                     <div className="flex flex-wrap justify-center gap-6">
+                        <FoundPetCard />
+                        <FoundPetCard />
+                        <FoundPetCard />
+                        <FoundPetCard />
 
-                        <FoundPetCard/>
-                        <FoundPetCard/>
-                        <FoundPetCard/>
-                        <FoundPetCard/>
+                        <FoundPetCard />
+                        <FoundPetCard />
+                        <FoundPetCard />
+                        <FoundPetCard />
 
-                        <FoundPetCard/>
-                        <FoundPetCard/>
-                        <FoundPetCard/>
-                        <FoundPetCard/>
-                        
                     </div>
                 </div>
             </div>
-            <button className="px-10 py-4 text-xl font-bold text-[#A60530] border border-white bg-[rgba(244,244,244,0.12)] rounded-full shadow-lg cursor-pointer hover:bg-[rgba(204,204,204,0.12)]">Report a Lost Pet</button>
+            <button className="px-10 py-4 text-xl font-bold text-[#A60530] border border-white bg-[rgba(244,244,244,0.12)] rounded-full shadow-lg cursor-pointer hover:bg-[rgba(204,204,204,0.12)] z-100">Report a Lost Pet</button>
         </div>
     )
 }
