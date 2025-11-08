427f900f2d9144f5e0becd81d1291dd1a0728905
Aaron John Bitara <118180256+eyronjohn@users.noreply.github.com>
2025-11-08 11:45:48 +0800

    Revert "small change"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,11 +6,7 @@ export default function Home() {
         <img src='/src/assets/—Pngtree—happy dog and cat with_15913340 2.png' id='pet-pic' alt='dogpic'/>
         <div id='home-container'>    
             <div className="home-info">
-                <div id='safepaws'>
-                    <h1> SafeP </h1>
-                    <img src='/src/assets/paw-print 4.png' alt='paw'/>
-                    <h1>ws</h1>
-                </div>
+                <h1> SafeP<span><img src='/src/assets/paw-print 4.png' alt='paw'/></span>ws</h1>
                 <p> Reuniting hearts, one paw at a time. To help all our pet owners in this community, even you, by joining us. </p>
                 <button id='register'>REGISTER NOW!</button>
             </div>
