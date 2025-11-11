8b2501d368bed7308216aeab3928948d749c51ec
Aaron John Bitara <118180256+eyron29@users.noreply.github.com>
2025-11-11 18:47:31 +0800

    minor changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import happyDogCat from '../../assets/—Pngtree—happy dog and cat with_15913340 2.png'
 
-function Header({title, subtitle}) {
+function Header({ title, subtitle }) {
     return (
-        <div className="w-full h-auto flex justify-between items-center bg-[rgba(255,255,255,0.21)] rounded-xl shadow-md relative">
-            <div className="flex flex-col px-4 py-2 gap-2 w-2/3">
-                <p className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-[#A60530]">{title}</p>
+        <div className="w-full h-auto flex flex-col sm:flex-row justify-between items-center bg-[rgba(255,255,255,0.21)] rounded-xl shadow-md relative">
+            <div className="text-center sm:text-left w-11/12 sm:w-2/3 flex flex-col px-4 py-2 gap-2">
+                <p className="text-5xl sm:text-6xl font-extrabold text-[#A60530]">{title}</p>
                 <p className='text-md sm:text-lg'>{subtitle}</p>
             </div>
-            <div className="w-1/4 md:w-1/5 h-52 md:h-32 flex justify-center items-center z-100 absolute right-0 bottom-0">
+            <div className="w-2/4 md:w-1/5 h-40 flex justify-center items-center">
                 <img src={happyDogCat} alt="Happy dog & cat" className="w-full h-full " />
             </div>
         </div>
--- src/components/Header2/Header2.jsx
+++ src/components/Header2/Header2.jsx
@@ -3,16 +3,16 @@ import happyDogCat from '../../assets/—Pngtree—happy dog and cat with_159133
 
 function Header2() {
     return (
-        <div className="w-full h-auto flex justify-between items-center bg-[rgba(255,255,255,0.21)] rounded-xl shadow-md">
-            <div className="flex flex-col px-4 w-2/3">
-                <div className="flex items-center tracking-tighter">
+        <div className="w-full h-auto flex flex-col sm:flex-row justify-between items-center bg-[rgba(255,255,255,0.21)] rounded-xl shadow-md">
+            <div className="text-center sm:text-left w-11/12 sm:w-2/3 flex flex-col px-4 py-2 gap-2">
+                <div className="flex justify-center sm:justify-start items-center tracking-tighter text-center sm:text-left">
                     <p className="text-5xl sm:text-6xl font-extrabold text-[#A60530]">SafeP</p>
                     <img src={pawPrintLogo} alt="Paw print logo" className="w-10 h-10 sm:w-12 sm:h-12 rotate-12 translate-y-2 sm:translate-y-3" />
                     <p className="text-5xl sm:text-6xl font-extrabold text-[#A60530]">ws</p>
                 </div>
                 <p className='text-md sm:text-lg font-semibold'>Reuniting hearts, one paw at a time.</p>
             </div>
-            <div className="w-1/4 sm:w-1/5 h-32 flex justify-center items-center z-100">
+            <div className="w-2/4 md:w-1/5 h-40 flex justify-center items-center">
                 <img src={happyDogCat} alt="Happy dog & cat" className="w-full h-full" />
             </div>
         </div>
