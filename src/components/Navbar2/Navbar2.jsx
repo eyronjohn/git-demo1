8010cc8243c7f2ef2f442f0db236c340a3bee85a
Aaron John Bitara <118180256+eyron29@users.noreply.github.com>
2025-11-12 18:41:46 +0800

    minor changes

--- src/components/Navbar2/Navbar2.jsx
+++ src/components/Navbar2/Navbar2.jsx
@@ -1,13 +1,23 @@
 import { Link, useLocation } from "react-router";
 import { useEffect, useState, useRef } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../../firebase-config";
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
 
 function Navbar2() {
     const [openDropdown, setOpenDropdown] = useState(false)
+    const [user, setUser] = useState(null);
     const location = useLocation();
-    const dropdownRef = useRef(null); 
+    const dropdownRef = useRef(null);
 
-    useEffect(()=>{
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    useEffect(() => {
         setOpenDropdown(false);
     }, [location]);
 
@@ -24,24 +34,58 @@ function Navbar2() {
         };
     }, []);
 
+    const handleLogout = () => {
+        signOut(auth);
+    };
+
     return (
-        <>
-            <div className="w-full h-12 py-6 flex items-center border border-[#F4F4F4] rounded-full shadow-lg bg-[rgba(244,244,244,0.12)]">
-                <div className="w-[90%] mx-auto flex justify-between items-center">
-                    <Link to="/" className="text-lg font-bold text-[#A60530]">SafePaws</Link>
-                    <div ref={dropdownRef} style={{ position: 'relative' }}>
-                    <button className="text-lg font-semibold cursor-pointer" onClick={()=>setOpenDropdown(prev => !prev)}>
+        <div className="w-full h-12 py-6 flex items-center border border-[#F4F4F4] rounded-full shadow-lg bg-[rgba(244,244,244,0.12)]">
+            <div className="w-[90%] mx-auto flex justify-between items-center">
+                <Link to="/" className="text-lg font-bold text-[#A60530] hover:underline">
+                    SafePaws
+                </Link>
+
+                <div ref={dropdownRef} style={{ position: "relative" }}>
+                    <button
+                        className="text-lg font-semibold cursor-pointer"
+                        onClick={() => setOpenDropdown((prev) => !prev)}
+                    >
                         Pet <i className="fa-solid fa-caret-down"></i>
                     </button>
                     {openDropdown && <DropdownMenu />}
-                    </div>
-                    <Link to="/about" className="text-lg font-semibold">About</Link>
-                    <Link to="/contactus" className="text-lg font-semibold">Contact</Link>
-                    <Link to="/login" className="text-lg font-semibold text-[#A60530]">Sign-In</Link>
                 </div>
+
+                <Link to="/about" className="text-lg font-semibold hover:underline">
+                    About
+                </Link>
+                <Link to="/contactus" className="text-lg font-semibold hover:underline">
+                    Contact
+                </Link>
+
+                {user ? (
+                    <div className="flex gap-2">
+                        <Link to="/profile">
+                            <img
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_lvjjRAVDQ-nBDq_4dy1xCyRjjDaHV-Tqcw&s"
+                                alt="Profile"
+                                className="w-9 h-9 rounded-full border-2 border-[#A60530] object-cover cursor-pointer"
+                            />
+                        </Link>
+                        {/* <button
+                            onClick={handleLogout}
+                            className="text-lg text-[#A60530] font-semibold cursor-pointer hover:underline "
+                        >
+                            Logout
+                        </button> */}
+                    </div>
+                ) : (
+                    <Link to="/login" className="text-lg font-semibold text-[#A60530]">
+                        Sign-In
+                    </Link>
+                )}
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
 export default Navbar2;
--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -10,29 +10,29 @@ function ProfileCard() {
     const [userData, setUserData] = useState(); // holds user data
     const navigate = useNavigate();
 
-      const handleLogOut = ()=> {
+    const handleLogOut = () => {
         auth.signOut().then(() => {
-                setTimeout(() => navigate("/login"), 0);
-            })
+            setTimeout(() => navigate("/login"), 0);
+        })
             .catch((error) => {
                 alert(error.message);
             });
     };
 
-    useEffect(()=>{
-        onAuthStateChanged(auth, (u)=>{
-        if(u){
-            setUser(u);
-            onValue(ref(db, `user/${u.uid}`),(snapshot)=>{
-                setUserData(snapshot.val());
-            })
-        }
-        else{
-            setUser(null);
-            setUserData(null);
-        }
+    useEffect(() => {
+        onAuthStateChanged(auth, (u) => {
+            if (u) {
+                setUser(u);
+                onValue(ref(db, `user/${u.uid}`), (snapshot) => {
+                    setUserData(snapshot.val());
+                })
+            }
+            else {
+                setUser(null);
+                setUserData(null);
+            }
         })
-    },[]);
+    }, []);
 
     return (
         <div className="w-full sm:w-[80%] md:w-[60%] lg:w-[50%] flex flex-col items-center gap-2 p-8 bg-[rgba(255,255,255,0.21)] rounded-3xl shadow-lg z-100">
@@ -44,28 +44,30 @@ function ProfileCard() {
                     <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" className=" w-full h-full rounded-full" />
                 </div>
                 {user && userData &&
-                <div className="flex flex-col justify-center items-start text-lg bg-white/10 backdrop-blur-md px-6 py-4 rounded-2xl shadow-md w-full max-w-md">
-                <p className="text-2xl font-semibold text-[#A60530] mb-3 text-center w-full">Profile Details</p>
-                <div className="space-y-2 text-[#222] w-full">
-                <p><span className="font-semibold text-[#A60530]">Full Name:</span> {userData.fullname}</p>
-                <p><span className="font-semibold text-[#A60530]">Username:</span> {userData.username}</p>
-                <p><span className="font-semibold text-[#A60530]">Email:</span> {userData.email}</p>
-                <p><span className="font-semibold text-[#A60530]">Contact Number:</span> {userData.contactNumber}</p>
-                <p><span className="font-semibold text-[#A60530]">Reported Lost Pets:</span> —</p>
-                <p><span className="font-semibold text-[#A60530]">Reported Found Pets:</span> —</p>
-            </div>
-        </div>
-        }
-                <Link to="/editprofile" className="px-12 py-2 text-xl font-bold text-[#A60530] border border-white bg-[rgba(244,244,244,0.12)] rounded-full shadow-lg cursor-pointer hover:bg-[rgba(204,204,204,0.12)]">Edit Profile</Link>
-                <button
-                onClick={handleLogOut}
-                className="px-12 py-2 mt-4 text-xl font-bold text-white bg-red-600 hover:bg-red-500 rounded-full shadow-lg transition-colors duration-200"
-                >
-                Log Out
-                </button>
+                    <div className="flex flex-col justify-center items-start text-lg bg-white/10 backdrop-blur-md px-6 py-4 rounded-2xl shadow-md w-full max-w-md">
+                        <p className="text-2xl font-semibold text-[#A60530] mb-3 text-center w-full">Profile Details</p>
+                        <div className="space-y-2 text-[#222] w-full">
+                            <p><span className="font-semibold text-[#A60530]">Full Name:</span> {userData.fullname}</p>
+                            <p><span className="font-semibold text-[#A60530]">Username:</span> {userData.username}</p>
+                            <p><span className="font-semibold text-[#A60530]">Email:</span> {userData.email}</p>
+                            <p><span className="font-semibold text-[#A60530]">Contact Number:</span> {userData.contactNumber}</p>
+                            <p><span className="font-semibold text-[#A60530]">Reported Lost Pets:</span> —</p>
+                            <p><span className="font-semibold text-[#A60530]">Reported Found Pets:</span> —</p>
+                        </div>
+                    </div>
+                }
+                <div className="flex gap-2">
+                    <Link to="/editprofile" className="px-12 py-2 text-lg sm:text-xl font-bold text-[#A60530] border border-white bg-[rgba(244,244,244,0.12)] rounded-full shadow-lg cursor-pointer hover:bg-[rgba(204,204,204,0.12)]">Edit Profile</Link>
+                    <button
+                        onClick={handleLogOut}
+                        className="px-12 py-2 mt-4 text-lg sm:text-xl font-bold text-[#F2C879] bg-[#A60530] rounded-full shadow-lg transition-colors duration-200 cursor-pointer"
+                    >
+                        Log Out
+                    </button>
                 </div>
             </div>
-        
+        </div>
+
     )
 }
 
